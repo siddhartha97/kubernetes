0596a7ed8ed839a8a2f06bd68258e839b79d7c2f
Siddhartha Chowdhuri <siddharthachowdhuri@gmail.com>
2021-03-03 11:59:06 -0800

    Added description for authentication types

--- staging/src/k8s.io/api/authentication/v1beta1/types.go
+++ staging/src/k8s.io/api/authentication/v1beta1/types.go
@@ -35,6 +35,8 @@ import (
 // plugin in the kube-apiserver.
 type TokenReview struct {
 	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#metadata
 	// +optional
 	metav1.ObjectMeta `json:"metadata,omitempty" protobuf:"bytes,1,opt,name=metadata"`
 
